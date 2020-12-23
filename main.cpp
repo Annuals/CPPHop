0c1d3038b2541a0b35a31090a044c9c363235bb7
LinkerScript <75097279+LinkerScript@users.noreply.github.com>
2020-12-23 02:45:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,95 @@
+
+// CHopper v2.102.32
+
 #include <iostream>
 #include <TlHelp32.h>
 #include <Windows.h>
 
+// Define the bhop key as C
 #define BHOP_KEY 'C'
 
+// Add a structure
 struct module_t
 {
   DWORD dwBase, dwSize;
 };
 
-//debugger class
+// The Debugger class
 
 class CDebugger
 {
+
 private:
   DWORD dwPid;
   HANDLE hProcess;
+  
 public:
+  
   bool attach(char* szProcess)
+    
   {
+    
     HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPRROCESS, NULL)
     PROCESSENTRY entry;
     entry.dwSize = sizeof(entry);
     do {
+      
       if (!strcmp(entry.szExeFile,szProcess))
       {
+        
         dwPid = entry.th32ProcessID;
         CloseHandle(handle) // Close the handle 
         hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, dwPid);
         return true;
       }
-    } while (Process32Next(handle, &entry));
+      
+    }
+    while (Process32Next(handle, &entry));
     return false
+      
   }
   
   module_t GetModule(char* szModule)
+    
   {
     HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPRROCESS, NULL)
     MODULEENTRY32 entry;
     entry.dwSize = sizeof(entry);
+    
     do {
       if (!strcmp(entry.szModule,szModule))
+      
       {
         dwPid = entry.th32ProcessID;
         CloseHandle(handle) // Close the handle 
         hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, dwPid);
         return{ (DWORD)entry.hModule, entry.ModBaseSize };
+     
       }
-    } while (Module32Next(handle, &entry));
+    
+    } 
+    while (Module32Next(handle, &entry));
     return false
+  
   }
+  
   
   template<typename T>
   T ReadMemory(DWORD Address)
+  
   {
     T read;
     ReadProcessMemory(hProcess, Address, &read, sizeof(T), 0);
+  
   }
   template<typename T>
   void WriteMemory(DWORD Address, T value)
+  
   {
     WriteProcessMemory(hProcess, Address, &val, sizeof(T), 0);
+  
   }
+
 };
 
 // offsets  
@@ -75,21 +103,28 @@ module_t moduleClient;
 
 // Locate the client dll in memory
 module_t* GetModuleClient()
+
 {
   if (!moduleClient.dwBase && !moduleClient.dwSize) {
     moduleClient = debugger.GetModule("client.dll");
+  
   }
   return &moduleClient;
+
 }
 
 
 class CBunnyhop
+
 {
+  
 public:
   static unsigned long __stdcall ThreadRoutine(void*)
+  
   {
     
     while (true)
+    
     {
       DWORD dwMe = debugger.ReadMemory<DWORD>(GetModuleClient()->dwBase + dwLocalPlayer);
       int CanJump = debugger.ReadMemory<DWORD>(dwMe + dwFlags);
@@ -98,19 +133,25 @@ public:
         Sleep(50);
         debugger.WriteMemory<int>(GetModuleClient()->dwBase + dwJump, 4);
       } 
+      
     }
+    
   }
+  
 };
 
 
 int main()
 {
+  
   while(!debugger.attach("csgo.exe")) {
     cout << Attaching to csgo.exe... << endl;
     Sleep(100);
   }
+  
   CreateThread(0,0,&CBunnyhop::ThreadRoutine, 0,0,0); // Create the bhop thread
   while (1) {
     Sleep(100);
   }
+  
 }
